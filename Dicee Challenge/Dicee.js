b792d1a037759d1282247784a197099fa57ef02a
Arun <141402276+Arun10112000@users.noreply.github.com>
2023-10-12 16:02:45 +0530

    Dicee Challenge Project

---
+++ Dicee Challenge/Dicee.js
@@ -0,0 +1,31 @@
+
+//coding for Image-1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;   //images/dice1.png -images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+//coding for Image-2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Win!"
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Win!"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!"
+}
+
+
