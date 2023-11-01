fe2528370b13cbe4c269fd34ba8e319ad4609d99
Arun <141402276+Arun10112000@users.noreply.github.com>
2023-11-01 19:16:00 +0530

    Add files via upload

---
+++ Tweet Box/TweetBox.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Compose your tweet:");
+var tweetCount = tweet.length;
+alert("You have writtern " + tweetCount + " Characters, you have " + ( 140 - tweetCount) + " Characters remaining.");
