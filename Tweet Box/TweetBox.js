0d99ad4ac872bf73734b35e20ab334e87469ae71
Arun <141402276+Arun10112000@users.noreply.github.com>
2023-11-04 06:48:38 +0530

    Update TweetBox.js

--- Tweet Box/TweetBox.js
+++ Tweet Box/TweetBox.js
@@ -1,3 +1,3 @@
 var tweet = prompt("Compose your tweet:");
 var tweetCount = tweet.length;
-alert("You have writtern " + tweetCount + " Characters, you have " + ( 140 - tweetCount) + " Characters remaining.");
+alert ("You have writtern " + tweetCount + " Characters, you have " + ( 140 - tweetCount) + " Characters remaining.");
